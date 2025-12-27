5a620b52a902b30a9c1a2a3ea13b5bc847a97df0
sourabhchat <sourabhchat@gmail.com>
2025-12-27 17:19:32 +0530

    Initial commit for Flask microservice

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api', methods=['GET'])
+def home():
+    return jsonify({'message': 'Hello, Kubernetes!'})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
